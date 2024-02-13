61572363f0df5b49d1ed20a820946f33f46533dd
skindin <90371126+skindin@users.noreply.github.com>
2024-02-12 19:31:20 -0700

    and again

--- postList.php
+++ postList.php
@@ -21,9 +21,12 @@ function getPosts($sortType, $sortOrder, $userId = -1)
         return false;
     }
 
+    // Create bind types string
+    $bindTypes = str_repeat('s', count($params) - 1) . 'i'; // 's' for string, 'i' for integer
+
     // Bind parameters
-    $bindTypes = str_repeat('s', count($params)) . 'i'; // 's' for string, 'i' for integer
-    mysqli_stmt_bind_param($stmt, $bindTypes, ...$params, $showCount);
+    mysqli_stmt_bind_param($stmt, $bindTypes, ...$params);
+
 
     // Execute statement
     mysqli_stmt_execute($stmt);
