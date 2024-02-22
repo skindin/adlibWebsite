7e604b5bb516b23c585b3c9f31591ad658ff15fe
skindin <90371126+skindin@users.noreply.github.com>
2024-02-21 17:59:43 -0700

    poob doob

--- vote.php
+++ vote.php
@@ -1,33 +1,32 @@
 <?php
-    function vote ($postId, $voteValue)
+    function vote($postId, $voteValue)
     {
         global $conn;
         $userId = $_SESSION['user']['userId'];
 
-        $sql = "SELECT from goodVotes WHERE postId = '".$postId."' AND userId = '".$userId;
-        if (!empty(mysqli_query($conn,$sql)))
-        {
-            echo 'This user already liked this post';
-        }
-        else
-        {
-            $sql = "INSERT INTO goodVotes (postId, userId, voteValue) VALUES ('".$postId."', '".$userId."', '".$voteValue."')";
+        // Check if the user has already voted for this post
+        $sql = "SELECT * FROM goodVotes WHERE postId = ? AND userId = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "ii", $postId, $userId);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
 
-            if (mysqli_query($conn,$sql))
-            {
+        if (mysqli_stmt_num_rows($stmt) > 0) {
+            echo 'This user already liked this post';
+        } else {
+            // Insert the vote into the database
+            $sql = "INSERT INTO goodVotes (postId, userId, voteValue) VALUES (?, ?, ?)";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "iii", $postId, $userId, $voteValue);
+
+            if (mysqli_stmt_execute($stmt)) {
                 echo 'Vote sent successfully';
                 return true;
+            } else {
+                echo 'Error: ' . mysqli_error($conn);
             }
         }
 
         return false;
-    }
-
-    if (isset($_POST['postId']) && isset($_POST['voteValue']))
-    {
-        $postId = $_POST['postId'];
-        $voteValue = $_POST['voteValue'];
-
-        vote($postId, $voteValue);
     }
 ?>
