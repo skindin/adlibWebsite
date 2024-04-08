ee808825b293f7e01010021ad5e87ec1827ef07d
skindin <90371126+skindin@users.noreply.github.com>
2024-04-07 20:08:45 -0600

    forgor to start session

--- vote.php
+++ vote.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+
     if (isset($_POST['postId']) && isset($_POST['voteValue'])) {
         // Retrieve the data sent from JavaScript
         $postId = $_POST['postId'];
