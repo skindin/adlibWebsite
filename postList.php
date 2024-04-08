37751370eee765e2e1c8bfca22e521ef97082468
skindin <90371126+skindin@users.noreply.github.com>
2024-04-07 20:47:42 -0600

    please work

--- postList.php
+++ postList.php
@@ -54,14 +54,14 @@ function printPost($post)
 
         echo '<button onclick="sendVote('.$postId.', 1,';
         echo $_SESSION['user']['userId'].', ';
-        echo $_SESSION['user']['password'];
+        echo "'".$_SESSION['user']['password']."'";
         echo ')">Good</button>';
 
         echo $goodness;
 
         echo '<button onclick="sendVote('.$postId.', -1,';
         echo $_SESSION['user']['userId'].', ';
-        echo $_SESSION['user']['password'];
+        echo "'".$_SESSION['user']['password']."'";
         echo ')">Bad</button>';
 
         echo '<br>Posted '.$timeStamp;
