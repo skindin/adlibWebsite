8d4c0e7a95101aad9602fef6524b6dd3b117da52
skindin <90371126+skindin@users.noreply.github.com>
2024-04-08 17:37:33 -0600

    maybe?

--- vote.php
+++ vote.php
@@ -1,6 +1,5 @@
 <?php
     include('database_check.php');
-    include('passwordCheck.php');
 
     if (isset($_POST['postId']) && isset($_POST['voteValue'])) {
         // Retrieve the data sent from JavaScript
